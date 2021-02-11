7b243ea41dd14f40e948050df047b12bc038d164
darrentsynthesis <darrent@synthesis.co.za>
2021-02-11 13:45:42 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-o29jy7-dkhw3v
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-o29jy7-dkhw3v)
---
+++ src/app/accordion/accordion.component.ts
@@ -0,0 +1,30 @@
+import { Component, ViewChild, ViewContainerRef } from '@angular/core';
+import { FieldType } from '@ngx-formly/core';
+
+@Component({
+  selector: 'app-accordion',
+  templateUrl: './accordion.component.html',
+  styleUrls: ['./accordion.component.scss']
+})
+export class AccordionComponent extends FieldType {
+  /**
+   * Callback to invoke when an active tab is collapsed by clicking on the header.
+   * @param $event Tabview event
+   */
+  onClose(event) {
+    if (this.to.onClose) {
+      this.to.onClose(event);
+    }
+  }
+
+  /**
+   * Callback to invoke when a tab gets expanded.
+   * @param $event Tabview event
+   */
+  onOpen(event) {
+    if (this.to.onOpen) {
+      this.to.onOpen(event);
+    }
+  }
+
+}
